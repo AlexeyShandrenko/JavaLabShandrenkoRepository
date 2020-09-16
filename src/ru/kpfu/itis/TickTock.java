ddd4c405977a43fadee2ee91c469c3c066cd4f12
AlexeyShandrenko <70972424+AlexeyShandrenko@users.noreply.github.com>
2020-09-17 00:11:37 +0300

    ThreadPoll's done!!

---
+++ src/ru/kpfu/itis/TickTock.java
@@ -0,0 +1,48 @@
+package ru.kpfu.itis;
+
+public class TickTock {
+
+    String state;
+
+    synchronized void tick(boolean running) {
+        if (!running) {
+            state = "ticked";
+            notify();
+            return;
+        }
+
+        System.out.println("Tick");
+
+        state = "ticked";
+        notify();
+        try {
+            while (!state.equals("tocked")) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Прерывание потока");
+        }
+    }
+
+    synchronized void tock(boolean running) {
+        if (!running) {
+            state = "tocked";
+            notify();
+            return;
+        }
+
+        System.out.println("Tock");
+
+        state = "tocked";
+
+        notify();
+        try {
+            while (!state.equals("ticked")) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Прерывание потока");
+        }
+    }
+
+}
