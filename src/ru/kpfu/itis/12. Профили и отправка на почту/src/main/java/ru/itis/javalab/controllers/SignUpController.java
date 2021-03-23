01b754c20a7f7f3014b2cf187beb623dc8d13973
AlexeyShandrenko <70972424+AlexeyShandrenko@users.noreply.github.com>
2021-03-23 19:44:04 +0300

    adding localization and validation

--- src/ru/kpfu/itis/12. Профили и отправка на почту/src/main/java/ru/itis/javalab/controllers/SignUpController.java
+++ src/ru/kpfu/itis/12. Профили и отправка на почту/src/main/java/ru/itis/javalab/controllers/SignUpController.java
@@ -3,6 +3,8 @@ package ru.itis.javalab.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,8 +15,10 @@ import ru.itis.javalab.services.UsersService;
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Objects;
 
 @Controller
 @Profile("master")
@@ -23,15 +27,33 @@ public class SignUpController {
     @Autowired
     private SignUpService signUpService;
 
+    @RequestMapping(value = "/notification", method = RequestMethod.GET)
+    public String getNotificationPage() throws IOException {
+        return "notification";
+    }
+
     @RequestMapping(value = "/sign_up", method = RequestMethod.GET)
-    public String getSignUpPage() throws IOException {
+    public String getSignUpPage(Model model) throws IOException {
+        model.addAttribute("userForm", new UserForm());
         return "login_window";
     }
 
     @RequestMapping(value = "/sign_up", method = RequestMethod.POST)
-    public String signUp(UserForm userForm) {
+    public String signUp(@Valid UserForm userForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().stream().anyMatch(error -> {
+                if (Objects.requireNonNull(error.getCodes())[0].equals("userForm.ValidNames")) {
+                    model.addAttribute("namesErrorMessage", error.getDefaultMessage());
+                }
+                return true;
+            });
+            model.addAttribute("userForm", userForm);
+            return "sign_up_page";
+        }
         signUpService.signUp(userForm);
-        return "redirect:/notification";
+        return "redirect:/success";
+
+    
     }
 
 }
